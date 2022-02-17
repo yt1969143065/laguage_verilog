90f0bb2eb0410df98ea4565c1f9998977d9f4531
yt1969143065 <yt1969143065@users.noreply.github.com>
2022-02-17 18:59:21 +0800

    Update decoder.v

--- decoder.v
+++ decoder.v
@@ -8,10 +8,16 @@ module decoder
   output  [LINES-1:0] unitary_out
 );
 
+//use generate
 genvar i;
 generate
 for (i = 0; i < LINES; i = i + 1) begin
   assign unitary_out[i] = binary_in == i;
 end
 endgenerate
+  
+//a more simple code
+/*
+assign unitary_out = {LINES'b1} << binary_in;
+*/
 endmodule
